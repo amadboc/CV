29e6be72604da156ee3ae7f9e2446647f94c0a99
amadboc <you@example.com>
2018-09-10 19:57:22 +0200

    cambios folio

--- Folio/js/main.js
+++ Folio/js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Ana Madero.", "Diseñadora On-Off.", "Maquetadora.","Creativa."],
       typeSpeed: 100,
       loop: true,
     });
